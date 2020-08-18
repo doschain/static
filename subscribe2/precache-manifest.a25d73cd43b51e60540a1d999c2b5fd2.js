8ef3bdff27902a8889524ed8fc2ed6eba715ea32
doschain <doschain@hotmail.com>
2020-08-18 15:06:33 +0800

    update

---
+++ subscribe2/precache-manifest.a25d73cd43b51e60540a1d999c2b5fd2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af835e4a18d85ccf1b220a6d4c0a7ffc",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/index.html"
+  },
+  {
+    "revision": "91c5b56f0a13fbf77cc1",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/css/0.3a3f5775.chunk.css"
+  },
+  {
+    "revision": "ad99a92f6311ae719469",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/css/3.650fd775.chunk.css"
+  },
+  {
+    "revision": "6cb15afb26d503c38cd0",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/css/4.193721d9.chunk.css"
+  },
+  {
+    "revision": "96a739db56c961d34d2b",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/css/5.fe34ab16.chunk.css"
+  },
+  {
+    "revision": "137a97ffb30e2a572990",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/css/6.d09485be.chunk.css"
+  },
+  {
+    "revision": "995fd5390f99386e058c",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/css/7.fa457439.chunk.css"
+  },
+  {
+    "revision": "c79a3b8dee28599e7590",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/css/8.13efd66a.chunk.css"
+  },
+  {
+    "revision": "14861f3d601e371bd5a0",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/css/main.01e70262.chunk.css"
+  },
+  {
+    "revision": "91c5b56f0a13fbf77cc1",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/js/0.16d50704.chunk.js"
+  },
+  {
+    "revision": "ad99a92f6311ae719469",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/js/3.3accf7eb.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/js/3.3accf7eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb15afb26d503c38cd0",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/js/4.d5b464e5.chunk.js"
+  },
+  {
+    "revision": "96a739db56c961d34d2b",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/js/5.867b702a.chunk.js"
+  },
+  {
+    "revision": "137a97ffb30e2a572990",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/js/6.e743b2e1.chunk.js"
+  },
+  {
+    "revision": "995fd5390f99386e058c",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/js/7.30a2cf2e.chunk.js"
+  },
+  {
+    "revision": "c79a3b8dee28599e7590",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/js/8.e984c831.chunk.js"
+  },
+  {
+    "revision": "14861f3d601e371bd5a0",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/js/main.113601bd.chunk.js"
+  },
+  {
+    "revision": "468fd82670b395d93e40",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/js/runtime-main.8f5439c9.js"
+  },
+  {
+    "revision": "740df8d8a04fb9b313ee317cc25de3b5",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/static/media/home_header_bg.740df8d8.png"
+  }
+]);
--- subscribe2/service-worker.js
+++ subscribe2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/doschain/static/subscribe/precache-manifest.c4c285f8899f15ec9ff6144222fdd642.js"
+  "https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/precache-manifest.a25d73cd43b51e60540a1d999c2b5fd2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://cdn.jsdelivr.net/gh/doschain/static/subscribe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://cdn.jsdelivr.net/gh/doschain/static/subscribe2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
