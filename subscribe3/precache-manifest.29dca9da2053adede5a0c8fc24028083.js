e41aff4ea9009e55954c6e10d7bd7552cb684d91
doschain <doschain@hotmail.com>
2020-08-19 14:05:37 +0800

    update

---
+++ subscribe3/precache-manifest.29dca9da2053adede5a0c8fc24028083.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d5b7a609d4c58e11125f48ab5ea1dd",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/index.html"
+  },
+  {
+    "revision": "e8f7b215040ddb61277d",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/css/0.3a3f5775.chunk.css"
+  },
+  {
+    "revision": "31f6d3d8902796941928",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/css/3.650fd775.chunk.css"
+  },
+  {
+    "revision": "648cc6d189d80f066635",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/css/4.049d6100.chunk.css"
+  },
+  {
+    "revision": "85169cd77b5037054334",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/css/5.fe34ab16.chunk.css"
+  },
+  {
+    "revision": "72e09c7ebaf44126764b",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/css/6.d6b98e16.chunk.css"
+  },
+  {
+    "revision": "92e7f8bf3b44739597af",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/css/7.fa457439.chunk.css"
+  },
+  {
+    "revision": "39c484b25ecc812af222",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/css/8.4bbdd75a.chunk.css"
+  },
+  {
+    "revision": "c037ccf7745105143523",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/css/main.22e718be.chunk.css"
+  },
+  {
+    "revision": "e8f7b215040ddb61277d",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/0.a8a0b3fc.chunk.js"
+  },
+  {
+    "revision": "31f6d3d8902796941928",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/3.922b1372.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/3.922b1372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "648cc6d189d80f066635",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/4.277a9b10.chunk.js"
+  },
+  {
+    "revision": "85169cd77b5037054334",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/5.a6779bcc.chunk.js"
+  },
+  {
+    "revision": "72e09c7ebaf44126764b",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/6.f9e403ed.chunk.js"
+  },
+  {
+    "revision": "92e7f8bf3b44739597af",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/7.e8539be4.chunk.js"
+  },
+  {
+    "revision": "39c484b25ecc812af222",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/8.c09b712e.chunk.js"
+  },
+  {
+    "revision": "08bf9267d4147fb8a876",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/9.bb4c05ab.chunk.js"
+  },
+  {
+    "revision": "c037ccf7745105143523",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/main.9af80362.chunk.js"
+  },
+  {
+    "revision": "74133228c710a9daa0af",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/js/runtime-main.72010431.js"
+  },
+  {
+    "revision": "740df8d8a04fb9b313ee317cc25de3b5",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/static/media/home_header_bg.740df8d8.png"
+  }
+]);
--- subscribe3/service-worker.js
+++ subscribe3/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/precache-manifest.5c335c5e83615df51ee7d566fd9c0c3f.js"
+  "https://cdn.jsdelivr.net/gh/doschain/static/subscribe3/precache-manifest.29dca9da2053adede5a0c8fc24028083.js"
 );
 
 self.addEventListener('message', (event) => {
