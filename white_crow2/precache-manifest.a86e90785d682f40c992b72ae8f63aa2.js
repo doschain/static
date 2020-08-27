2434723d9cd3d67eae0d3992df8b6c9c613f5e6c
doschain <doschain@hotmail.com>
2020-08-27 19:41:55 +0800

    update

---
+++ white_crow2/precache-manifest.a86e90785d682f40c992b72ae8f63aa2.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d050907e066a14da4a10bcf53c685b",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/index.html"
+  },
+  {
+    "revision": "8072137484286746c1af",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/css/4.6908fd7b.chunk.css"
+  },
+  {
+    "revision": "01ae13786fb21dcaaa1e",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/css/5.142a631e.chunk.css"
+  },
+  {
+    "revision": "b76a5c00ae7369f61a3d",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/css/6.d6311b21.chunk.css"
+  },
+  {
+    "revision": "46fe45666c41e14aba92",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/css/main.44c3e1f5.chunk.css"
+  },
+  {
+    "revision": "2fae8d202940938de435",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/js/2.1a2f0c6e.chunk.js"
+  },
+  {
+    "revision": "d9bee4e26266dab8796c9ed1573a3e49",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/js/2.1a2f0c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db2acdbf56e96a5e424",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/js/3.b4be5580.chunk.js"
+  },
+  {
+    "revision": "8072137484286746c1af",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/js/4.07211838.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/js/4.07211838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01ae13786fb21dcaaa1e",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/js/5.4c2ecff1.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/js/5.4c2ecff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b76a5c00ae7369f61a3d",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/js/6.97203db5.chunk.js"
+  },
+  {
+    "revision": "46fe45666c41e14aba92",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/js/main.82313256.chunk.js"
+  },
+  {
+    "revision": "3634e009902034f64ec3",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/js/runtime-main.37e54d81.js"
+  },
+  {
+    "revision": "9b8a6d79a91c8ca88ef24b9f4612f0da",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/media/D-DINExp.9b8a6d79.ttf"
+  },
+  {
+    "revision": "9f8df4742286576ad37ab23f8a27cc34",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/media/home_bg.9f8df474.gif"
+  },
+  {
+    "revision": "25f731e14df8636943e6309e290751cf",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/media/home_bg2.25f731e1.png"
+  },
+  {
+    "revision": "999297a82caa8b1b73e662b179fb9493",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/media/home_bg3.999297a8.png"
+  },
+  {
+    "revision": "bfb57e5e8856b527a9efdfd7fd3a7ae8",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/media/m_bg_2.bfb57e5e.png"
+  },
+  {
+    "revision": "1aa4abbf791cb377480b64c6d8dc3e7d",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/static/media/m_bg_3.1aa4abbf.png"
+  }
+]);
--- white_crow2/service-worker.js
+++ white_crow2/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/precache-manifest.f686ebc7a1770cf96c2f3043ee645d0c.js"
+  "https://cdn.jsdelivr.net/gh/doschain/static/white_crow2/precache-manifest.a86e90785d682f40c992b72ae8f63aa2.js"
 );
 
 self.addEventListener('message', (event) => {
