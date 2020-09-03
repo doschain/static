8ed5afa6078b35629177d4446a33fd362e0489ea
doschain <doschain@hotmail.com>
2020-09-03 15:11:31 +0800

    add destroy dapp

---
+++ destroy/precache-manifest.90ff14f238b9e782e5c30ef2b94c8136.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816a875004799e70ce92ab16af85a0de",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/destroy/index.html"
+  },
+  {
+    "revision": "cba7f692e7c772949df3",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/destroy/static/css/3.8ce676b0.chunk.css"
+  },
+  {
+    "revision": "8e818dae27b02ec839b7",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/destroy/static/css/main.9115635f.chunk.css"
+  },
+  {
+    "revision": "6bf9df010b3a9348309e",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/destroy/static/js/2.66c18482.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/destroy/static/js/2.66c18482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba7f692e7c772949df3",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/destroy/static/js/3.b1939509.chunk.js"
+  },
+  {
+    "revision": "8e818dae27b02ec839b7",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/destroy/static/js/main.87c4290b.chunk.js"
+  },
+  {
+    "revision": "c50d342b8dbcaf17195a",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/destroy/static/js/runtime-main.82fb10d0.js"
+  },
+  {
+    "revision": "9b8a6d79a91c8ca88ef24b9f4612f0da",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/destroy/static/media/D-DINExp.9b8a6d79.ttf"
+  },
+  {
+    "revision": "40582d12d6593ea02e77827c2e9a9c78",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/destroy/static/media/bg.40582d12.png"
+  }
+]);
