36bdf2f96617148aa8e09bec7e17038028e4969c
doschain <doschain@hotmail.com>
2020-10-03 15:13:00 +0800

    新增dapp聚合平台cdn资源

---
+++ dapp_collection_test/precache-manifest.86e2ed1a4ed1308ec1d92c03ffd8a9b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a693e18a8b3f65deaae874578391c6",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/dapp_collection_test/index.html"
+  },
+  {
+    "revision": "207bd9e106e1a585e71a",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/dapp_collection_test/static/css/main.b2e4826e.chunk.css"
+  },
+  {
+    "revision": "445c1ae7077fb40f9dd0",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/dapp_collection_test/static/js/2.f8e6e47c.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/dapp_collection_test/static/js/2.f8e6e47c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "207bd9e106e1a585e71a",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/dapp_collection_test/static/js/main.b4981dac.chunk.js"
+  },
+  {
+    "revision": "c69c08b196dd968e4fbe",
+    "url": "https://cdn.jsdelivr.net/gh/doschain/static/dapp_collection_test/static/js/runtime-main.57c5d291.js"
+  }
+]);
